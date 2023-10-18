139a60aeee4b0142cdfcc5c8ad03573c97d08336
Sahil <leviack629@gmail.com>
2023-10-18 23:45:18 +0530

    components

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
