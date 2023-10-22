892e5b1f4145cc050b8fb96618ade28fc256b3cf
Sahil <leviack629@gmail.com>
2023-10-22 21:28:55 +0530

    components

---
+++ src/components/post-form/PostForm.jsx
@@ -0,0 +1,23 @@
+import React, { useCallback } from "react";
+import { useForm } from "react-hook-form";
+import { Button, Input, Select, RTE } from "./index";
+import appwriteService from "../../appwrite/config";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+const PostForm = () => {
+  const { register, handleSubmit, watch, setValue, control, getValues } =
+    useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    });
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.user.userData)
+  return <div>PostForm</div>;
+};
+
+export default PostForm;
